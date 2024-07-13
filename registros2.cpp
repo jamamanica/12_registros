57426f76f6b33bcd68d97ef8b8cff6cbb14a8df3
jamamanica <jamamanica@unjbg.edu.pe>
2024-07-12 21:19:43 -0500

    Establece promedio de edades e imprime los datos ingresados

--- registros2.cpp
+++ registros2.cpp
@@ -16,5 +16,22 @@ int main(){
         cout<<"DNI: ";cin>>datos[i].dni;
         cout<<"Edad: ";cin>>datos[i].edad;
     }
+    float c=0,s=0,p;
+    for(int i=0;i<n;i++){
+        if(datos[i].edad>50){
+            c++;
+        }
+        s=s+datos[i].edad;
+    }
+    p=s/n;
+    cout<<"--------------------------------"<<endl;
+    cout<<"Promedio de edades: "<<p<<endl;
+    cout<<"--------------------------------"<<endl;
+    cout<<"--Datos de personas ingresadas--"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<"Nombre: "<<datos[i].nombre<<endl;
+        cout<<"DNI: "<<datos[i].dni<<endl;
+        cout<<"Edad: "<<datos[i].edad<<endl;
+    }
     return 0;
 }
