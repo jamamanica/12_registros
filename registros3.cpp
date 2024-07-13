852e7cf45e473f84f1117a5ae7bb880e0fe4dd3d
jamamanica <jamamanica@unjbg.edu.pe>
2024-07-13 01:16:01 -0500

    Establece los meses en comun y agrega validaciones

--- registros3.cpp
+++ registros3.cpp
@@ -18,5 +18,22 @@ int main(){
         cout<<"Mes de nacimiento: ";cin>>datos[i].mes;
         cout<<"Anio de nacimiento: ";cin>>datos[i].anio;
     }
+    int cumple;
+    cout<<"-----------------------------------"<<endl;
+    string mes[12]={"enero","febrero","marzo","abril","mayo","junio","julio","agosto","setiembre","octubre","noviembre","diciembre"};
+    do{
+        cout<<"Ingrese numero de mes: ";cin>>cumple;
+        for(int i=0;i<n;i++){
+            if(mes[cumple-1]==datos[i].mes){
+                cout<<"--Personas que cumplen en ese mes--"<<endl;
+                cout<<"Nombre: "<<datos[i].nombre<<endl;
+                cout<<"Dia de nacimiento: "<<datos[i].dia<<endl;
+                cout<<"Mes de nacimiento: "<<datos[i].mes<<endl;
+                cout<<"Anio de nacimiento: "<<datos[i].anio<<endl;
+                break;
+            } 
+        }
+        cout<<"-----------------------------------"<<endl;
+    }while(cumple!=0&&cumple<=12);
     return 0;
 }
