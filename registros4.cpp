24d64bfe7e429ba223becab03f8f169825330b88
jamamanica <jamamanica@unjbg.edu.pe>
2024-07-13 02:24:04 -0500

    Establece los provinientes de cierto pais y el mayor en medallas

--- registros4.cpp
+++ registros4.cpp
@@ -18,5 +18,24 @@ int main(){
             cout<<"Disciplina: ";cin>>datos[i].disciplina;
             cout<<"Medallas: ";cin>>datos[i].medallas;
     }
+    string pais,name;
+    cout<<"------------------------------------------------"<<endl;
+    cout<<"Nombra un pais: ";cin>>pais;
+    int mayor=0;
+    for(int i=0;i<n;i++){
+        if(datos[i].pais==pais){
+            cout<<"Nombre: "<<datos[i].nombre<<endl;
+            cout<<"Pais: "<<datos[i].pais<<endl;
+            cout<<"Disciplina: "<<datos[i].disciplina<<endl;
+            cout<<"Medallas: "<<datos[i].medallas<<endl;
+            if(datos[i].medallas>mayor){
+                mayor=datos[i].medallas;
+                name=datos[i].nombre;
+            }
+        }
+    }
+    cout<<"------------------------------------------------"<<endl;
+    cout<<"--> El atleta con mayor # de medallas en "<<pais<<" es: "<<name<<endl;
+    cout<<"------------------------------------------------"<<endl;
     return 0;
 }
